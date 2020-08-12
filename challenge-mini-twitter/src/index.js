f9ad06c08e41842123ebd255ea3f25a39c240f58
Sadat <sadat0akhtar@gmail.com>
2020-08-12 14:14:38 +0100

    added delete icon functionality, tweet deleted when icon is clicked

--- challenge-mini-twitter/src/index.js
+++ challenge-mini-twitter/src/index.js
@@ -1,4 +1,7 @@
-let charLimit = 100;
+let charLimit = 10;
+let user = "@John";
+let imgTag;
+let aTag;
 
 //SELECTORS
 let submitButton = document.getElementById('submitBtn');
@@ -6,27 +9,34 @@ let tweetOutput = document.querySelector('.tweetOutput');
 let textArea = document.getElementById('myTextArea');
 let counterDiv = document.querySelector('.counter');
 
-
-
 //EVENT LISTENERS
 submitButton.addEventListener('click', function(e){
     e.preventDefault();
     let newParagraphTag = document.createElement('p');
-    let imgTag = document.createElement('img');
+    imgTag = document.createElement('img');
+    aTag = document.createElement('a');
+    aTag.id="myA";
+    aTag.href="#";
+    aTag.innerText = "@User";
     newParagraphTag.id="tweetP";
     imgTag.id="icon";
     imgTag.src = "./images/delete.svg";
 
     if(textArea.value.length <= charLimit){
-        newParagraphTag.innerText = textArea.value;
+        newParagraphTag.innerText = `${user}- ${textArea.value}`;
         tweetOutput.appendChild(newParagraphTag);
         newParagraphTag.appendChild(imgTag);
-       
+    
     }else{
         document.getElementById('cc').style.color="red";
     }
     textArea.value = "";
     setTimeout(resetCharCounter, 1000);
+
+    //EVENT LISTENER FOR DELETE ICON
+    imgTag.onclick = () =>{
+        newParagraphTag.innerText = "";
+    }
 });
 //RESET COUNTER 
 function resetCharCounter(){
@@ -35,7 +45,7 @@ function resetCharCounter(){
 };
 //CHECK CHARACTER LIMIT AND HIGHLIGHT COLOR
 function cc(obj){
-    if(obj.value.length > 100){
+    if(obj.value.length > charLimit){
         document.getElementById('newCCParagraph').innerHTML = obj.value.length+ ' characters';
         document.getElementById('newCCParagraph').style.color = "red";
     }else {
